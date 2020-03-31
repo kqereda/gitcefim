59cb8797fb9b1af0dac63b413b21d064d631bdd2
kqereda <46049847+kqereda@users.noreply.github.com>
2020-03-31 09:15:41 +0200

    premier commit pour l'exercice

---
+++ navigation.js
@@ -0,0 +1,10 @@
+const toggleMenu = document.querySelector('.navigation button');
+const menu = document.querySelector('.navigation ul');
+const body = document.querySelector('body');
+
+toggleMenu.addEventListener('click', function() {
+	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
+	toggleMenu.setAttribute('aria-expanded', !open);
+	menu.hidden = !menu.hidden;
+	body.classList.toggle("noscroll");
+});
